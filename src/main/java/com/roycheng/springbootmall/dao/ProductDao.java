fb0e6327e994e12680da7b9274b12f529785c677
Dreamer23090 <dillon28506@gmail.com>
2023-02-06 20:59:00 +0800

    商品功能 - 實作查詢商品列表(基礎實作)

--- src/main/java/com/roycheng/springbootmall/dao/ProductDao.java
+++ src/main/java/com/roycheng/springbootmall/dao/ProductDao.java
@@ -4,7 +4,11 @@ package com.roycheng.springbootmall.dao;
 import com.roycheng.springbootmall.dto.ProductRequest;
 import com.roycheng.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+
+    List<Product> getProducts();
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
--- src/main/java/com/roycheng/springbootmall/service/ProductService.java
+++ src/main/java/com/roycheng/springbootmall/service/ProductService.java
@@ -3,7 +3,11 @@ package com.roycheng.springbootmall.service;
 import com.roycheng.springbootmall.dto.ProductRequest;
 import com.roycheng.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getProducts();
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
